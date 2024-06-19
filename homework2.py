a3b082ae422b836918838077bafb986b2bbff3c1
Vlasig <vlasig274@gmail.com>
2024-06-19 18:58:34 +0300

    Initial commit

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_completed_homework = 12
+number_hours_spent = 1.5
+name_course = 'Python'
+average_time = number_hours_spent / number_completed_homework
+print('Курс:' ,name_course, ', всего задач:' ,number_completed_homework, ', затрачено часов:' ,number_hours_spent, ', среднее время выполнения' ,average_time, 'часа.')
